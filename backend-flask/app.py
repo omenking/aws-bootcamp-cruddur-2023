281ba0a0a35f3302cb435c7e449b92fcc98b0095
Andrew Brown <andrew@monsterboxpro.com>
2023-02-22 22:07:54 +0000

    configuring cloudwatch logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -39,6 +39,13 @@ provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
+# CloudWatch Logs -----
+import watchtower
+import logging
+from time import strftime
+
+
+
 app = Flask(__name__)
 
 # HoneyComb -------
@@ -61,6 +68,23 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+
+# CloudWatch --------
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("some message")
+
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
 
 @app.route('/rollbar/test')
 def rollbar_test():
@@ -122,6 +146,7 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
   data = HomeActivities.run()
+  LOGGER.info('Hello Cloudwatch! from  /api/activities/home')
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
--- journal/week2.md
+++ journal/week2.md
@@ -170,6 +170,61 @@ gp env HONEYCOMB_SERVICE_NAME="Cruddur"
 
 ## CloudWatch Logs
 
+
+Add to the `requirements.txt`
+
+```
+watchtower
+```
+
+```sh
+pip install -r requirements.txt
+```
+
+
+In `app.py`
+
+```
+import watchtower
+import logging
+from time import strftime
+```
+
+```py
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("some message")
+```
+
+```py
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
+```
+
+We'll log something in an API endpoint
+```py
+LOGGER.info('Hello Cloudwatch! from  /api/activities/home')
+```
+
+Set the env var in your backend-flask for `docker-compose.yml`
+
+```yml
+      AWS_REGION: "${AWS_REGION}"
+```
+
+```sh
+export AWS_REGION="ca-central-1"
+gp env AWS_REGION="ca-central-1"
+```
+
 ## Rollbar
 
 https://rollbar.com/
