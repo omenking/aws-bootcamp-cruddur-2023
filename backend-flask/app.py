d840fc0897e6babe68c21b29af9746c06687037f
njt0 <nicat.hummetov1@gmail.com>
2023-03-07 12:36:01 +0100

    week-2

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -26,6 +26,20 @@ from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProces
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+#CLoudWATCH
+import watchtower
+import logging
+from time import strftime
+
+# Configuring Logger to Use CloudWatch
+#LOGGER = logging.getLogger(__name__)
+#LOGGER.setLevel(logging.DEBUG)
+#console_handler = logging.StreamHandler()
+#cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+#LOGGER.addHandler(console_handler)
+#LOGGER.addHandler(cw_handler)
+#LOGGER.info("test log")
+
 
 
 #Honey tracing 
@@ -34,8 +48,8 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 
 #X-ray
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
+#xray_url = os.getenv("AWS_XRAY_URL")
+#xray_recorder.configure(service='Cruddur', dynamic_naming=xray_url)
 
 
 #LogComment
@@ -47,7 +61,7 @@ tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 
-XRayMiddleware(app, xray_recorder)
+#XRayMiddleware(app, xray_recorder)
 #Honey 
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
@@ -63,6 +77,12 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+#@app.after_request
+#def after_request(response):
+#    timestamp = strftime('[%Y-%b-%d %H:%M]')
+#    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#    return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
