bcb0af388524ee0393154ca713d4d799c6e2bfbf
Peter Victor <peter.24953@eng.modern-academy.edu.eg>
2023-03-10 14:05:36 +0000

    update app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -44,13 +44,15 @@ provider.add_span_processor(processor)
 # X-RAY ----------
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
 
 # HoneyComb
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+# X-RAY ----------
+XRayMiddleware(app, xray_recorder)
+
 # HONEYCOMB
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
