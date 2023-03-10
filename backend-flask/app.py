6228c0a7871843ad2eda6a4bec41f6e648afbd5c
Peter Victor <peter.24953@eng.modern-academy.edu.eg>
2023-03-10 14:38:13 +0000

    Disable X-Ray and CloudWatch

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -40,14 +40,14 @@ import watchtower
 import logging
 from time import strftime
 
-# Configuring Logger to Use CloudWatch
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
-console_handler = logging.StreamHandler()
-cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
-LOGGER.addHandler(console_handler)
-LOGGER.addHandler(cw_handler)
-LOGGER.info("some message")
+# Configuring Logger to Use CloudWatch ---------- Disabled to save on spend
+# LOGGER = logging.getLogger(__name__)
+# LOGGER.setLevel(logging.DEBUG)
+# console_handler = logging.StreamHandler()
+# cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+# LOGGER.addHandler(console_handler)
+# LOGGER.addHandler(cw_handler)
+# LOGGER.info("some message")
 
 # HONEYCOMB 
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -56,16 +56,16 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 
 # X-RAY ----------
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+# xray_url = os.getenv("AWS_XRAY_URL")
+# xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
 # HoneyComb
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
-# X-RAY ----------
-XRayMiddleware(app, xray_recorder)
+# X-RAY ---------- Disabled to save on spend
+# XRayMiddleware(app, xray_recorder)
 
 
 # HONEYCOMB
@@ -84,12 +84,12 @@ cors = CORS(
     methods="OPTIONS,GET,HEAD,POST"
 )
 
-# Error Logging
-@app.after_request
-def after_request(response):
-    timestamp = strftime('[%Y-%b-%d %H:%M]')
-    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
-    return response
+# Error Logging ---------- Disabled to save on spend
+# @app.after_request
+# def after_request(response):
+#     timestamp = strftime('[%Y-%b-%d %H:%M]')
+#     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#     return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
