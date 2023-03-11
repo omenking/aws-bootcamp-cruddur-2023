2d6712a7967e6e566abca278285e4fb785134fc9
Andrew Brown <andrew@monsterboxpro.com>
2023-03-11 22:30:09 +0000

    post confirmation lambda

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -5,9 +5,10 @@ DROP TABLE IF EXISTS public.activities;
 
 CREATE TABLE public.users (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  display_name text,
-  handle text,
-  cognito_user_id text,
+  display_name text NOT NULL,
+  handle text NOT NULL,
+  email text NOT NULL,
+  cognito_user_id text NOT NULL,
   created_at TIMESTAMP default current_timestamp NOT NULL
 );
 
