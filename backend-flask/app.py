9062cf07dcfbf37da332992901eb6cc440c8042b
Peter Victor <peter.24953@eng.modern-academy.edu.eg>
2023-03-11 08:31:43 +0000

    Integrate Rollbar For Error Logging

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -40,6 +40,11 @@ import watchtower
 import logging
 from time import strftime
 
+# ROLLBAR
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 # Configuring Logger to Use CloudWatch ---------- Disabled to save on spend
 # LOGGER = logging.getLogger(__name__)
 # LOGGER.setLevel(logging.DEBUG)
@@ -64,6 +69,24 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+# ROLLBAR --------
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        'f6bc037f315a43729231fa200ec1f86f',
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
 # X-RAY ---------- Disabled to save on spend
 # XRayMiddleware(app, xray_recorder)
 
@@ -90,6 +113,12 @@ cors = CORS(
 #     timestamp = strftime('[%Y-%b-%d %H:%M]')
 #     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #     return response
+
+# ROLLBAR TEST
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
