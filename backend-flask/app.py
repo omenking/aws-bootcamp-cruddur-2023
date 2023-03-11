a86af4b19caac01732d87ac98fac2c6999b3f65c
Peter Victor <peter.24953@eng.modern-academy.edu.eg>
2023-03-11 08:50:34 +0000

    Add Rollbar access token env

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -174,7 +174,7 @@ def data_notifications():
 
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
-@xray_recorder.capture('activities_users')
+#@xray_recorder.capture('activities_users')
 def data_handle(handle):
     model = UserActivities.run(handle)
     if model['errors'] is not None:
@@ -209,7 +209,7 @@ def data_activities():
 
 
 @app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
-@xray_recorder.capture('activities_show')
+#@xray_recorder.capture('activities_show')
 def data_show_activity(activity_uuid):
     data = ShowActivity.run(activity_uuid=activity_uuid)
     return data, 200
