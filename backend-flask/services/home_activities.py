624db49bdd36cbc6e1292074b6fdcfae6a541ae7
marcuswong <cswkmab@gmail.com>
2023-03-17 19:11:42 +0000

    puhs query working local

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-from lib.db import pool
+from lib.db import pool,query_wrap_array
 
 #tracer = trace.get_tracer("home.activities")
 
@@ -15,15 +15,28 @@ class HomeActivities:
       
     #   span.set_attribute("app.now", now.isoformat())
 
-      sql= f"""
-      SELECT * FROM activities
-      """
+      sql= query_wrap_array("""
+        SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
+      """)
 
       with pool.connection() as conn:
         with conn.cursor() as cur:
           cur.execute(sql)
           # this will return a tuple
           # the first field being the data
-          json = cur.fetchall()
+          json = cur.fetchone()
       return json[0]
       return results
