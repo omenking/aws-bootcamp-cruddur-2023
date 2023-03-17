c8a9fe305a3276bb187b5e2895c3b542b2bfa23e
marcuswong <cswkmab@gmail.com>
2023-03-17 18:51:15 +0000

    db setup wip

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,70 +1,29 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-tracer = trace.get_tracer("home.activities")
+from lib.db import pool
+
+#tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run(cognito_user_id=None):
   #def run(logger):
     # logger.info("HomeActivities")
-    with tracer.start_as_current_span("home-activities-mock-data"):
-      span = trace.get_current_span()
-      now = datetime.now(timezone.utc).astimezone()
+    # with tracer.start_as_current_span("home-activities-mock-data"):
+    #   span = trace.get_current_span()
+    #   now = datetime.now(timezone.utc).astimezone()
       
-      span.set_attribute("app.now", now.isoformat())
-      results = [{
-        'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
-        'created_at': (now - timedelta(days=2)).isoformat(),
-        'expires_at': (now + timedelta(days=5)).isoformat(),
-        'likes_count': 5,
-        'replies_count': 1,
-        'reposts_count': 0,
-        'replies': [{
-          'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
-          'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-          'handle':  'Worf',
-          'message': 'This post has no honor!',
-          'likes_count': 0,
-          'replies_count': 0,
-          'reposts_count': 0,
-          'created_at': (now - timedelta(days=2)).isoformat()
-        }],
-      },
-      {
-        'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-        'handle':  'Worf',
-        'message': 'I am out of prune juice',
-        'created_at': (now - timedelta(days=7)).isoformat(),
-        'expires_at': (now + timedelta(days=9)).isoformat(),
-        'likes': 0,
-        'replies': []
-      },
-      {
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Garek',
-        'message': 'My dear doctor, I am just simple tailor',
-        'created_at': (now - timedelta(hours=1)).isoformat(),
-        'expires_at': (now + timedelta(hours=12)).isoformat(),
-        'likes': 0,
-        'replies': []
-      }
-      ]
+    #   span.set_attribute("app.now", now.isoformat())
 
-  
-      if cognito_user_id != None:
-        extra_crud = {
-          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-          'handle':  'Lore',
-          'message': 'My dear brother, it the humans that are the problem',
-          'created_at': (now - timedelta(hours=1)).isoformat(),
-          'expires_at': (now + timedelta(hours=12)).isoformat(),
-          'likes': 1042,
-          'replies': []
-        }
-        results.insert(0,extra_crud)
-      
-      span.set_attribute("app.result_length", len(results))
+      sql= f"""
+      SELECT * FROM activities
+      """
 
-    return results
+      with pool.connection() as conn:
+        with conn.cursor() as cur:
+          cur.execute(sql)
+          # this will return a tuple
+          # the first field being the data
+          json = cur.fetchall()
+      return json[0]
+      return results
