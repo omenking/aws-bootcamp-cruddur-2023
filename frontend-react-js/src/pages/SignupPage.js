10df32628f520d517c6576b26451c1700e456493
Peter Victor <56364669+Peter2220@users.noreply.github.com>
2023-03-20 21:05:07 +0200

    Update SignupPage.js

--- frontend-react-js/src/pages/SignupPage.js
+++ frontend-react-js/src/pages/SignupPage.js
@@ -4,7 +4,8 @@ import {ReactComponent as Logo} from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
 
 // [TODO] Authenication
-import Cookies from 'js-cookie'
+// import Cookies from 'js-cookie'
+import { Auth } from 'aws-amplify';
 
 export default function SignupPage() {
 
@@ -14,7 +15,7 @@ export default function SignupPage() {
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [errors, setErrors] = React.useState('');
-
+/*
   const onsubmit = async (event) => {
     event.preventDefault();
     console.log('SignupPage.onsubmit')
@@ -27,6 +28,33 @@ export default function SignupPage() {
     window.location.href = `/confirm?email=${email}`
     return false
   }
+ */
+  
+  const onsubmit = async (event) => {
+    event.preventDefault();
+    setErrors('')
+    try {
+      const { user } = await Auth.signUp({
+        username: email,
+        password: password,
+        attributes: {
+          name: name,
+          email: email,
+          preferred_username: username,
+        },
+        autoSignIn: { // optional - enables auto sign in after user is confirmed
+          enabled: true,
+        }
+      }) ;
+      console.log(user);
+      window.location.href = `/confirm?email=${email}`
+    } catch (error) {
+        console.log(error);
+        setErrors(error.message)
+    }
+    return false
+  }
+
 
   const name_onchange = (event) => {
     setName(event.target.value);
