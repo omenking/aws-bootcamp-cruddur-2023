27f7d7e132080732d7543c4e837dfee0216bcb01
Peter Victor <56364669+Peter2220@users.noreply.github.com>
2023-03-20 21:45:59 +0200

    Update RecoverPage.js

--- frontend-react-js/src/pages/RecoverPage.js
+++ frontend-react-js/src/pages/RecoverPage.js
@@ -2,6 +2,7 @@ import './RecoverPage.css';
 import React from "react";
 import {ReactComponent as Logo} from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
+import { Auth } from 'aws-amplify';
 
 export default function RecoverPage() {
   // Username is Eamil
@@ -14,12 +15,23 @@ export default function RecoverPage() {
 
   const onsubmit_send_code = async (event) => {
     event.preventDefault();
-    console.log('onsubmit_send_code')
+    setErrors('')
+    Auth.forgotPassword(username)
+    .then((data) => setFormState('confirm_code') )
+    .catch((err) => setErrors(err.message) );
     return false
   }
+  
   const onsubmit_confirm_code = async (event) => {
     event.preventDefault();
-    console.log('onsubmit_confirm_code')
+    setErrors('')
+    if (password == passwordAgain){
+      Auth.forgotPasswordSubmit(username, code, password)
+      .then((data) => setFormState('success'))
+      .catch((err) => setErrors(err.message) );
+    } else {
+      setCognitoErrors('Passwords do not match')
+    }
     return false
   }
 
@@ -108,7 +120,7 @@ export default function RecoverPage() {
 
   const success = () => {
     return (<form>
-      <p>Your password has been successfully reset!</p>
+      <p style="color:White;">Your password has been successfully reset!</p>
       <Link to="/signin" className="proceed">Proceed to Signin</Link>
     </form>
     )
