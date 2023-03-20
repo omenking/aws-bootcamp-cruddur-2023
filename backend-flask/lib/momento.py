a187076b195ed1d9788138c6184ce37bcdfa3834
Kirk Kirkconnell <synchrophoto@gmail.com>
2023-03-20 13:45:25 -0700

    Streamline momento.py

--- backend-flask/lib/momento.py
+++ backend-flask/lib/momento.py
@@ -6,42 +6,33 @@ import os
 
 class MomentoCounter:
   def client():
-    print("MomentoCounter.client")
     momento_auth_token = CredentialProvider.from_environment_variable('MOMENTO_AUTH_TOKEN')
-    momento_ttl_seconds = os.getenv('MOMENTO_TTL_SECONDS')
-    ttl  = timedelta(seconds=int(momento_ttl_seconds))
-
-    print("<><><><><><>")
-    print(os.getenv('MOMENTO_AUTH_TOKEN'))
-
+    ttl  = timedelta(int(os.getenv('MOMENTO_TTL_SECONDS')))
     config = {
       'configuration': Configurations.Laptop.v1(),
       'credential_provider': momento_auth_token,
       'default_ttl':  ttl
     }
-    print(config)
     return CacheClient(**config)
   # get value if not present then reset
-  def get(key):
+  def get(cache_name, key):
     cache_name = os.getenv('MOMENTO_CACHE_NAME')
     with MomentoCounter.client() as client:
       resp = client.get(cache_name, key)
       if isinstance(resp, CacheGet.Hit):
-        return int(resp.value_string) 
+        return resp.value_string
       elif isinstance(resp, CacheGet.Miss):
         raise "missed"
       elif isinstance(resp, CacheGet.Error):
         raise resp.inner_exception
-  #increment
-  def incr(key):
-    cache_name = os.getenv('MOMENTO_CACHE_NAME')
+  #increment the value of a key
+  def incr(cache_name, key, value = 1):
     with MomentoCounter.client() as client:
-      print("MomentoCounter.incr ==")
-      resp = client.increment(cache_name,key,amount=1)
+      resp = client.increment(cache_name,key,value)
       print(resp)
 
   # reset to zero
-  def reset(key):
+  def reset(cache_name, key):
     cache_name = os.getenv('MOMENTO_CACHE_NAME')
     with MomentoCounter.client() as client:
       resp = client.set(cache_name, key, 0)
