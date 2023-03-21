5ba87878644597fd97ef5b92ae798add1a224d3d
Peter Victor <peter.24953@eng.modern-academy.edu.eg>
2023-03-21 12:04:39 +0000

    Fix xray

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,43 +1,46 @@
 from datetime import datetime, timedelta, timezone
-from opentelemetry import trace
+from aws_xray_sdk.core import xray_recorder
 
-tracer = trace.get_tracer("user-activities")
+
 class UserActivities:
-  def run(user_handle):
-    with tracer.start_as_current_span("user-data"):
-      span = trace.get_current_span()
-      model = {
-        'errors': None,
-        'data': None
-      }
+    def run(user_handle):
+        try:
+            with xray_recorder.capture('user_activity_time') as subsegment_1:
+                model = {
+                    'errors': None,
+                    'data': None
+                }
 
-      now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("user.now", now.isoformat())
-      
-      if user_handle == None or len(user_handle) < 1:
-        model['errors'] = ['blank_user_handle']
-      else:
-        now = datetime.now()
-        span.set_attribute("user.now", now.isoformat())
-        span.set_attribute("UserID", user_handle)
-        results = [{
-          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-          'handle':  'Andrew Brown',
-          'message': 'Cloud is fun!',
-          'created_at': (now - timedelta(days=1)).isoformat(),
-          'expires_at': (now + timedelta(days=31)).isoformat()
-        }]
-        model['data'] = results
+                now = datetime.now(timezone.utc).astimezone()
 
-      subsegment = xray_recorder.begin_subsegment('mock-data')
-      # xray ---
-      dict = {
-        "now": now.isoformat(),
-        "results-size": len(model['data'])
-      }
-      subsegment.put_metadata('key', dict, 'namespace')
-      xray_recorder.end_subsegment()
-    finally:  
-     # Close the segment
-      xray_recorder.end_subsegment()
-    return model
+                dict = {
+                    "now": now.isoformat(),
+                    "user_handle": user_handle,
+                }
+
+                # Add metadata or annotation to sub segment-1 if necessary
+                subsegment_1.put_metadata(
+                    'user_metadata_key', dict, 'namespace')
+                subsegment_1.put_annotation('time_now', now.isoformat())
+
+                if user_handle == None or len(user_handle) < 1:
+                    model['errors'] = ['blank_user_handle']
+                else:
+                    now = datetime.now()
+                    results = [{
+                        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+                        'handle':  'Andrew Brown',
+                        'message': 'Cloud is fun!',
+                        'created_at': (now - timedelta(days=1)).isoformat(),
+                        'expires_at': (now + timedelta(days=31)).isoformat()
+                    }]
+                    model['data'] = results
+
+            # Add metadata or annotation to subsegment 2 if necessary
+            sub_segment = xray_recorder.begin_subsegment(
+                "user_data.subsegment")
+            sub_segment.put_annotation('user.result.length', len(results))
+
+        finally:
+            xray_recorder.end_subsegment()
+        return model
