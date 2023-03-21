30f0a78721b7f221043f924501030a40286ba7ad
Peter Victor <56364669+Peter2220@users.noreply.github.com>
2023-03-21 13:07:53 +0200

    Honeycomb Custom instrumentation UserID

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,18 +1,25 @@
 from datetime import datetime, timedelta, timezone
+from opentelemetry import trace
+
+tracer = trace.get_tracer("user-activities")
 class UserActivities:
   def run(user_handle):
-    try:
+    with tracer.start_as_current_span("user-data"):
+      span = trace.get_current_span()
       model = {
         'errors': None,
         'data': None
       }
 
       now = datetime.now(timezone.utc).astimezone()
+      span.set_attribute("user.now", now.isoformat())
       
       if user_handle == None or len(user_handle) < 1:
         model['errors'] = ['blank_user_handle']
       else:
         now = datetime.now()
+        span.set_attribute("user.now", now.isoformat())
+        span.set_attribute("UserID", user_handle)
         results = [{
           'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
           'handle':  'Andrew Brown',
