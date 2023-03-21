2d23baf4949fc1da2eea3618689fa6ac7ab51f9c
Peter Victor <peter.24953@eng.modern-academy.edu.eg>
2023-03-21 07:49:26 +0000

    Fix backend

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -1,8 +1,8 @@
+import os
+import sys
 from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
-import os
-import sys
 
 from services.home_activities import *
 from services.notifications_activities import *
