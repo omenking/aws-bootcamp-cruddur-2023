d3dbd590aa0c4ed1f8612935d1857ba9249364f4
marcuswong <cswkmab@gmail.com>
2023-04-01 21:10:53 +0000

    implement fargate backned. Starting on frontend

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -147,6 +147,10 @@ cors = CORS(
 #     rollbar.report_message('Hello World!', 'warning')
 #     return "Hello World!"
 
+@app.route('/api/health-check')
+def health_check():
+  return {'success': True}, 200
+  
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
   access_token = extract_access_token(request.headers)
