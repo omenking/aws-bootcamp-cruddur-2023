5d323b1d344580401ff75b232668e37e2d4d45c1
Andrew Brown <andrew@monsterboxpro.com>
2023-04-03 15:20:27 +0000

    wip

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -14,20 +14,28 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     super(scope, id, props);
     
     const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
+    const folderInput: string = process.env.S3_FOLDER_INPUT as string;
+    const folderOutput: string = process.env.S3_FOLDER_OUTPUT as string;
     const webhookUrl: string = process.env.WEBHOOK_URL as string;
     const topicName: string = process.env.TOPIC_NAME as string;
+    const functionPath: string = path.join(__dirname, '..', '..', '..', 'aws', 'lambdas','process-images')
 
     const bucket = this.createBucket(bucketName)
-    const lambda = this.createLambda(bucketName)
+    const lambda = this.createLambda(functionPath,bucketName)
 
-    // Grant the Lambda function access to the S3 bucket
+    // This could be redundent, could be creating bucket policy?
     bucket.grantRead(lambda);
     bucket.grantPut(lambda);
 
     const snsTopic = this.createSnsTopic(topicName)
     const snsSubscription = this.createSnsSubscription(snsTopic,webhookUrl)
+    const lambdaNotification = this.createS3NotifyToLambda(lambda,snsTopic,bucket)
 
-    const lambdaNotification = this.createS3NotifyToLambda(lambda,bucket)
+    const s3ReadWritePolicy = this.createPolicyBucketAccess(bucket.bucketArn)
+    const snsPublishPolicy = this.createPolicySnSPublish(snsTopic.topicArn)
+
+    lambda.addToRolePolicy(s3ReadWritePolicy);
+    lambda.addToRolePolicy(snsPublishPolicy);
   }
 
   /*
@@ -46,10 +54,9 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     return bucket;
   }
 
-  createLambda(bucketName: string): lambda.IFunction {
-    const code = lambda.Code.fromAsset(path.join(__dirname, '..', '..', '..', 'aws', 'lambdas','process-images'))
-
+  createLambda(functionPath: string, bucketName: string): lambda.IFunction {
     const logicalName = 'ThumbLambda';
+    const code = lambda.Code.fromAsset(functionPath)
     const lambdaFunction = new lambda.Function(this, logicalName, {
       runtime: lambda.Runtime.NODEJS_18_X,
       handler: 'index.handler',
@@ -61,17 +68,17 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     return lambdaFunction;
   }
 
-  createS3NotifyToLambda(lambda: lambda.IFunction, bucket: s3.IBucket): s3n.ILambdaDestination {
+  createS3NotifyToLambda(folderInput: string, lambda: lambda.IFunction, bucket: s3.IBucket): s3n.ILambdaDestination {
     const lambdaNotification = new s3n.LambdaDestination(lambda);
     bucket.addEventNotification(s3.EventType.OBJECT_CREATED, new s3n.NotificationKeyFilter({
-      prefix: 'raw/',
+      prefix: folderInput,
     }), lambdaNotification);
     return lambdaNotification;
   }
 
-  createS3NotifyToSns(lambda: lambda.IFunction, bucket: s3.IBucket): s3n.ILambdaDestination {
+  createS3NotifyToSns(folderOutput: string, snsTopic: sns.ITopic, bucket: s3.IBucket): s3n.ILambdaDestination {
     bucket.addEventNotification(s3.EventType.OBJECT_CREATED, new s3n.NotificationKeyFilter({
-      prefix: 'thumbnails/',
+      prefix: folderOutput,
     }), new s3n.SnsDestination(snsTopic));
   }
 
@@ -91,5 +98,30 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
       endpoint: webhookUrl
     });
     return snsSubscription;
+  }
+
+  createPolicyBucketAccess(bucketArn: string){
+    const s3ReadWritePolicy = new iam.PolicyStatement({
+      actions: [
+        's3:GetObject',
+        's3:PutObject',
+      ],
+      resources: [
+        `${bucketArn}/*`,
+      ]
+    });
+    return s3ReadWritePolicy;
+  }
+
+  createPolicySnSPublish(topicArn: string){
+    const snsPublishPolicy = new iam.PolicyStatement({
+      actions: [
+        'sns:Publish',
+      ],
+      resources: [
+        topicArn
+      ]
+    });
+    return snsPublishPolicy;
   }
 }
