9948a425dd97d0506adf4df91c0d0a252a004989
Andrew Brown <andrew@monsterboxpro.com>
2023-05-24 16:08:13 +0000

    add dynamodb table column

--- backend-flask/lib/ddb.py
+++ backend-flask/lib/ddb.py
@@ -16,7 +16,7 @@ class Ddb:
     return dynamodb
   def list_message_groups(client,my_user_uuid):
     year = str(datetime.now().year)
-    table_name = 'cruddur-messages'
+    table_name = os.getenv("DDB_MESSAGE_TABLE")
     query_params = {
       'TableName': table_name,
       'KeyConditionExpression': 'pk = :pk AND begins_with(sk,:year)',
@@ -47,7 +47,7 @@ class Ddb:
     return results
   def list_messages(client,message_group_uuid):
     year = str(datetime.now().year)
-    table_name = 'cruddur-messages'
+    table_name = os.getenv("DDB_MESSAGE_TABLE")
     query_params = {
       'TableName': table_name,
       'KeyConditionExpression': 'pk = :pk AND begins_with(sk,:year)',
@@ -87,7 +87,7 @@ class Ddb:
       'user_handle': {'S': my_user_handle}
     }
     # insert the record into the table
-    table_name = 'cruddur-messages'
+    table_name = os.getenv("DDB_MESSAGE_TABLE")
     response = client.put_item(
       TableName=table_name,
       Item=record
@@ -103,7 +103,7 @@ class Ddb:
       'created_at': created_at
     }
   def create_message_group(client, message,my_user_uuid, my_user_display_name, my_user_handle, other_user_uuid, other_user_display_name, other_user_handle):
-    table_name = 'cruddur-messages'
+    table_name = os.getenv("DDB_MESSAGE_TABLE")
 
     message_group_uuid = str(uuid.uuid4())
     message_uuid = str(uuid.uuid4())
