9635b8bd86350025adcad348a78301628a69f1fc
Coding-Pinata <130298554+Coding-Pinata@users.noreply.github.com>
2023-11-04 16:25:00 +0900

    02 00 distributed tracing (#8)
    * #7 Instrumented HoneyComb in the backend
    
    * #7 Customised the HoneyComb tracing

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,7 +14,34 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+
+# HoneyComb
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+
+# Initialize tracing and an exporter that can send data to Honeycomb
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter()) 
+provider.add_span_processor(processor)
+
+simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(simple_processor)
+
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
 app = Flask(__name__)
+
+# HoneyComb
+# Initialise automatic instrumentation with Flask
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
